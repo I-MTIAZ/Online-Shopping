31217fde7521439ff3a0be8d51a44142e8f2075f
Muhammad Imtiaz <109730605+I-MTIAZ@users.noreply.github.com>
2023-03-22 23:41:29 +0600

    initial commit

---
+++ javascript project/mysc.js
@@ -0,0 +1,25 @@
+/* let x = 0;
+function myfunction(){
+    if(x == 0){
+        const data = document.getElementById("demo").innerHTML = "Wa'laikum Assalam";
+        x = 1;
+    }
+    else{
+        const data = document.getElementById("demo").innerHTML = "Assalmu Assalam";
+        x = 0;
+    }
+    console.log(data);
+    //window.alert("Every thing is alright");
+} */
+
+
+
+
+
+    
+
+
+
+
+
+
